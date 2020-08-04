c42b2c907e9f22bbb52f22d99697a10e9a6ac35c
Tyler Hodges <Hodges3902@gmail.com>
2020-08-04 12:47:47 -0400

    Add project files.

---
+++ HowMuchMoney/Bank.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HowMuchMoney
+{
+    public class Bank
+    {
+        static string[] coins = { "penny", "nickel", "dime", "quarter" };
+        Random rand;
+
+        public Bank()
+        {
+            rand = new Random();
+        }
+
+        public Bank( int RandomSeed )
+        {
+            rand = new Random(RandomSeed);
+        }
+
+        public int CountMyCoins()
+        {
+            return rand.Next(10, 50);
+        }
+
+        public string[] GetCoins( int howMany )
+        {
+            string[] MyCoins = new string[howMany];
+
+            for (int i = 0; i < howMany; i++)
+            {
+                MyCoins[i] = coins[rand.Next(0, 4)];
+            }
+
+            return MyCoins;
+        }
+
+        public double HowMuch( string[] AllTheCoins )
+        {
+            double total = 0;
+
+            for (int i = 0; i < AllTheCoins.Length; i++)
+            {
+                if(AllTheCoins[i] == "penny")
+                {
+                    total += .01;
+                }
+                if (AllTheCoins[i] == "nickel")
+                {
+                    total += .05;
+                }
+                if (AllTheCoins[i] == "dime")
+                {
+                    total += .10;
+                }
+                if (AllTheCoins[i] == "quarter")
+                {
+                    total += .25;
+                }
+            }
+
+            return total;
+        }
+    }
+}
---
+++ HowMuchMoney/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HowMuchMoney
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank MyBank = new Bank( 73 );
+            int numOfCoins = MyBank.CountMyCoins();
+
+            Console.WriteLine($"Wow! I have {numOfCoins} coins in my bank!\n");
+
+            string[] AllMyCoins = MyBank.GetCoins(numOfCoins);
+            double howMuch = MyBank.HowMuch(AllMyCoins);
+            Console.WriteLine($"I have ${Math.Round(howMuch)}\n");
+        }
+    }
+}
